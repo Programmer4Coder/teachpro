0ef7611d33451f71e2be4dd7dbad5ff0c95ff729
Akash Bisht <122256935+Programmer4Coder@users.noreply.github.com>
2024-12-12 22:35:48 +0530

    Delete techpro directory

--- techpro/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(max_length=100, required=True)
-    phone = forms.CharField(max_length=15, required=True)
-    question = forms.CharField(widget=forms.Textarea, required=True)
